62d39eeca999589c5733cc4219f219bad128da76
huypq <huyphamkma@gmail.com>
2021-10-04 20:06:48 +0700

    update

--- app.js
+++ app.js
@@ -56,6 +56,6 @@ route(app)
 
 
 
-app.listen(process.env.PORT || 3000, function(){
+app.listen(port, function(){
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
   });
